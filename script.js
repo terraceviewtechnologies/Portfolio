d00c456bee2ec2234eaa50d6067a75f2cb922197
terraceviewtechnologies <terraceviewtech@gmail.com>
2026-03-12 20:27:51 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function myFunction() {
+  var x = document.getElementById("navbar");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
